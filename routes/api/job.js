6c7697527126014ed4fc1cb7dc33962d3c7582ff
sanjib biswas <sanjib_biswas2002@MacBook-Pro.local>
2020-02-10 19:26:38 -0600

    corrected typo mistake

--- routes/api/job.js
+++ routes/api/job.js
@@ -1,5 +1,5 @@
 const router = require("express").Router();
-const jobsController = require("../../controllers/jobssController");
+const jobsController = require("../../controllers/jobsController");
 
 // Matches with "/api/jobs"
 router.route("/")
